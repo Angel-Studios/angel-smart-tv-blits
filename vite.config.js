42809879a24bb4775bd17c48fdd9f960187651ef
Fernando Ramirez <16779411+FernandoX7@users.noreply.github.com>
2025-02-28 14:08:39 -0600

    Add support for Chromium 38 >=

--- vite.config.js
+++ vite.config.js
@@ -17,11 +17,22 @@
 
 import { defineConfig } from 'vite'
 import blitsVitePlugins from '@lightningjs/blits/vite'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig(({ command, mode, ssrBuild }) => {
+  const isProduction = mode === 'production'
+
   return {
     base: '/', // Set to your base path if you are deploying to a subdirectory (example: /myApp/)
-    plugins: [...blitsVitePlugins],
+    plugins: [
+      ...blitsVitePlugins,
+      legacy({
+        // Ensure compatibility with Chromium 38
+        targets: ['chrome >= 38'],
+        modernPolyfills: true,
+        additionalLegacyPolyfills: ['whatwg-fetch'],
+      }),
+    ],
     resolve: {
       mainFields: ['browser', 'module', 'jsnext:main', 'jsnext'],
     },
@@ -36,6 +47,12 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
     },
     worker: {
       format: 'es',
+    },
+    build: {
+      target: isProduction ? 'es2019' : 'es2020',
+    },
+    esbuild: {
+      target: isProduction ? 'es2019' : 'es2020',
     },
   }
 })
